a75eb9a11cec723478d1d20b09bdae7bfaefeab3
MontreePuttan <montree@mintel.tech>
2019-11-24 23:14:47 +0700

    Integrate Data-Table

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import Header from './Header';
+import Menu from './Menu';
+import Footer from './Footer';
+import Content from './Content';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Demo React on Firebase</p>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header/>
+      <Menu/>
+      <Content/>
+      <Footer/>
     </div>
   );
 }
